ff54f48f59b92df18aba308d2ce840e88d168131
SravaniJava <sravanigoudjava@gmail.com>
2024-07-09 11:14:36 +0530

    test of parent or master

---
+++ maveneclipsesample/src/main/java/nit/maveneclipsesample/Testchild1branch.java
@@ -0,0 +1,10 @@
+package nit.maveneclipsesample;
+
+public class Testchild1branch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.print("testing of maven child");
+	}
+
+}
