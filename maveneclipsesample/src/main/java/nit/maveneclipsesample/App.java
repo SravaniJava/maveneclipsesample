7c384bd1ede6163bbf37724c8ef32ca19100d873
SravaniJava <sravanigoudjava@gmail.com>
2024-07-09 11:05:23 +0530

    working with maveneclishsamplerepo

---
+++ maveneclipsesample/src/main/java/nit/maveneclipsesample/App.java
@@ -0,0 +1,14 @@
+package nit.maveneclipsesample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("this is maven eclipse eg");
+    }
+}
