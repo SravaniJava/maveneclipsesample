ce509b262dddaeae62306aaeaea74ac68e7e76d1
SravaniJava <sravanigoudjava@gmail.com>
2024-07-09 11:44:00 +0530

    this is for 2nd time child testing

--- maveneclipsesample/src/main/java/nit/maveneclipsesample/Testchild1branch.java
+++ maveneclipsesample/src/main/java/nit/maveneclipsesample/Testchild1branch.java
@@ -5,6 +5,8 @@ public class Testchild1branch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.print("testing of maven child");
+     System.out.println("this is for testchild1branch");
+     System.out.print("HI This is my child branch 2nd attempt");
 	}
 
 }
