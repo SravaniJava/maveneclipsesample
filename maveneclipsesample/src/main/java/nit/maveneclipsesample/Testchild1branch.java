df01b5e875fc95696efd8f33cdab4cd506eb4724
SravaniJava <sravanigoudjava@gmail.com>
2024-07-10 11:58:04 +0530

    just for test of third time

--- maveneclipsesample/src/main/java/nit/maveneclipsesample/Testchild1branch.java
+++ maveneclipsesample/src/main/java/nit/maveneclipsesample/Testchild1branch.java
@@ -8,5 +8,5 @@ public class Testchild1branch {
      System.out.println("this is for testchild1branch");
      System.out.print("HI This is my child branch 2nd attempt");
      System.out.println("this is for raising merge conflits");
-	}
+     System.out.println("this is from test of nd time child1 branch");	}
 }
